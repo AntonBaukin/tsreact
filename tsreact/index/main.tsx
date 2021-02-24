d44b6b2adba72bd2a58d76616f0e88ede54a59d0
Anton Baukin <anton.baukin@gmail.com>
2021-02-24 22:34:51 +0300

    react.ts Moved Main under main (not to mess with case sensitivity).

--- tsreact/index/main.tsx
+++ tsreact/index/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom'
-import Main from '../src/Main'
+import Main from '../src/main/Main'
 
 document.addEventListener('DOMContentLoaded', () => {
 	ReactDOM.render(<Main />, document.body.querySelector('#main'))
